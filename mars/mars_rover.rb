ff7ec93d2c57765db05ebb9a7725093008dbb68c
Wendelcorp <bryce@brycewendelaar.com>
2016-08-11 17:52:42 -0400

    rover works

---
+++ mars/mars_rover.rb
@@ -0,0 +1,90 @@
+class Rover
+  attr_accessor :positionx
+  attr_accessor :positiony
+  attr_accessor :direction
+
+  def initialize(pos_x, pos_y, dir)
+    @positionx = pos_x
+    @positiony = pos_y
+    @direction = dir.upcase
+  end
+
+  def instruction(inst)
+    if inst == "L"
+      spin_left
+    elsif inst == "R"
+      spin_right
+    elsif inst == "M"
+      move
+    else
+      return "No instruction given"
+    end
+  end
+
+  def instructions(input)
+    input.each_char do |move|
+      instruction(move)
+    end
+  end
+
+  def move
+    if @direction == "N"
+      @positiony += 1
+    elsif @direction == "E"
+      @positionx += 1
+    elsif @direction == "S"
+      @positiony -= 1
+    else #              "W"
+      @positionx -= 1
+    end
+  end
+
+  def spin_left
+    if @direction == "N"
+      @direction = "W"
+    elsif @direction == "E"
+      @direction = "N"
+    elsif @direction == "S"
+      @direction = "E"
+    else #              "W"
+      @direction = "S"
+    end
+  end
+
+  def spin_right
+    if @direction == "N"
+      @direction = "E"
+    elsif @direction == "E"
+      @direction = "S"
+    elsif @direction == "S"
+      @direction = "W"
+    else #              "W"
+      @direction = "N"
+    end
+  end
+
+  def show_position
+    if @positionx.to_i > 9 || @positiony.to_i > 9
+      return "Rover lost connection - out of bounds"
+    elsif @positionx.to_i < 0 || @positiony.to_i < 0
+      return "Rover lost connection - out of bounds"
+    else
+      return @positionx, @positiony, @direction
+    end
+  end
+end
+
+rover1 = Rover.new(1, 1, "n")
+
+# rover1.move
+# rover1.move
+# rover1.spin_right
+# rover1.spin_right
+# rover1.move
+#
+# puts rover1.show_position
+
+puts rover1.instructions("LMRMM")
+
+
+puts rover1.show_position
